fc2430b97197e60ada4c0120a6f906a524bbae35
Daniel Targonski <20912079+Avanith@users.noreply.github.com>
2020-12-25 15:12:24 -0500

    modularized

--- main.cpp
+++ main.cpp
@@ -11,65 +11,83 @@
 
 using namespace std;
 
+/** Checks if inputted string is a postfix expression
+@pre none
+@post calculates inputted postfix expression and displays result to user.*/
+void checkPostfix();
+
 int main()
+{
+	checkPostfix();
+} // end main
+
+void checkPostfix()
 {
 	string postfix{};
 	int i{};
 	float result{}, operand1{}, operand2{};
 	LinkedStack<float> operandStack;
-	
-	cout << "Input a postfix expression: ";
-	cin >> postfix;
-	cout << "\n\n";
+	bool _continue{ true };
 
-	// Parses the postfix expression to evaluate the
-	// value of the postfix expression
-	for (i = 0; i < postfix.length(); i++)
+	while (_continue)
 	{
-		if (postfix[i] >= '0' && postfix[i] <= '9')
-		{
-			//push the value of the operand ch onto the stack
-			operandStack.push(postfix[i] - '0');
-		} // end if
+		cout << "Input a postfix expression or enter 'x' to exit: ";
+		cin >> postfix;
+		cout << "\n\n";
+
+		if (postfix == "x")
+			_continue = false;
 		else
 		{
-			// Evaluate and push the result
-			operand2 = operandStack.peek();
-			operandStack.pop();
-			operand1 = operandStack.peek();
-			operandStack.pop();
-
-			// Perform the correct operation on the operands
-			// and store it in result, then push result
-			// onto the stack
-			switch (postfix[i])
+			// Parses the postfix expression to evaluate the
+		// value of the postfix expression
+			for (i = 0; i < postfix.length(); i++)
 			{
-			case '+':
-				result = operand1 + operand2;
-				break;
-			case '-':
-				result = operand1 - operand2;
-				break;
-			case '*':
-				result = operand1 * operand2;
-				break;
-			case '/':
-				if (operand2 == 0) // cannot divide by zero
+				if (postfix[i] >= '0' && postfix[i] <= '9')
 				{
-					cout << "Undefined.\nCannot divide by zero.\n";
-					return 0;
+					//push the value of the operand ch onto the stack
+					operandStack.push(postfix[i] - '0');
 				} // end if
-				result = operand1 / operand2;
-				break;
+				else
+				{
+					// Evaluate and push the result
+					operand2 = operandStack.peek();
+					operandStack.pop();
+					operand1 = operandStack.peek();
+					operandStack.pop();
 
-			default:
-				break;
-			}
-			operandStack.push(result);
-		} // end else
-	} // end for loop
+					// Perform the correct operation on the operands
+					// and store it in result, then push result
+					// onto the stack
+					switch (postfix[i])
+					{
+					case '+':
+						result = operand1 + operand2;
+						break;
+					case '-':
+						result = operand1 - operand2;
+						break;
+					case '*':
+						result = operand1 * operand2;
+						break;
+					case '/':
+						if (operand2 == 0) // cannot divide by zero
+						{
+							cout << "Undefined.\nCannot divide by zero.\n\n";
+							break;
+						} // end if
+						result = operand1 / operand2;
+						break;
 
-	cout << "The result is: " << operandStack.peek() << "\n";
-
-	return 0;
-} // end main
+					default:
+						break;
+					}
+					operandStack.push(result);
+				} // end else
+			} // end for loop
+			cout << "The result is: " << operandStack.peek() << "\n\n";
+		}
+		
+	}
+	
+}
